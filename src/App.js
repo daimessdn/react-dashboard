a18737dbb23443b3c5997786bd68d612af1c38aa
daimessdn@gmail.com <daimessdn@gmail.com>
2022-05-30 11:40:35 +0700

    added necessary components and pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { FiSettings } from "react-icons/fi";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
+import { Navbar, Footer, Sidebar, ThemeSettings } from "./components";
+import {
+  ECommerce, Orders, Calendar, Empolyees, Stacked, Pyramid, Customers, Kanban,
+  Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor
+} from "./pages";
+
 import "./App.css";
 
 function App() {
